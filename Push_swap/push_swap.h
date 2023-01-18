85135b88e36344b35290e399593750013c68b126
Caos <camillalourenco@outlook.com>
2023-01-18 02:04:36 +0000

    header

---
+++ Push_swap/push_swap.h
@@ -0,0 +1,17 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+void swap(int *a, int *b);
+void sa(int *a, int size);
+void sb(int *b, int size);
+void ss(int *a, int *b, int size_a, int size_b);
+void pa(int *a, int *b, int *size_a, int *size_b);
+void pb(int *a, int *b, int *size_a, int *size_b);
+void ra(int *a, int size);
+void rb(int *b, int size);
+void rr(int *a, int *b, int size_a, int size_b);
+void rra(int *a, int size);
+void rrb(int *b, int size);
+void rrr(int *a, int *b, int size_a, int size_b);
+
+#endif
